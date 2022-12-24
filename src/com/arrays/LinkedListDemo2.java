23c74238f32a5fffecce897d50465523fd787695
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-24 21:42:34 +0530

    Second code: for linkedList

---
+++ src/com/arrays/LinkedListDemo2.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo2 {
+public static void main(String[] args) {
+	
+	LinkedList<Integer> ll2= new LinkedList<Integer>();
+	ll2.add(34);
+	ll2.add(45);
+	ll2.add(55);
+	System.out.println(ll2);
+}
+}
