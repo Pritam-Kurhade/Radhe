d56b00312d315c512ae5d2ef72df36187c31e5a0
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-24 21:31:49 +0530

    first commit:code

---
+++ src/com/arrays/AraryListDemo.java
@@ -0,0 +1,13 @@
+package com.arrays;
+
+import java.util.ArrayList;
+
+public class AraryListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("Pritam");
+		al.add("Suraj");
+		System.out.println(al);
+	}
+}
---
+++ src/com/arrays/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ll= new LinkedList<Integer>();
+		ll.add(3);
+		ll.add(5);
+		ll.add(6);
+		System.out.println(ll);
+	}
+}
